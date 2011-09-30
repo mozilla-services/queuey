d14e16ef43945ee50814dd0c72a17cce0935cf4c
Ben Bangert <ben@groovie.org>
2011-09-30 09:28:01 -0700

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,33 @@
+VIRTUALENV = virtualenv
+PYTHON = bin/python
+PIP = bin/pip
+EZ = bin/easy_install
+NOSE = bin/nosetests -s --with-xunit
+
+
+.PHONY:	all env cassandra
+
+all:	env
+
+env:
+	rm -rf bin build deps include lib lib64
+	$(VIRTUALENV) --no-site-packages --distribute .
+	$(PYTHON) setup.py develop
+
+clean-env:
+	rm -rf bin build include lib lib64S
+
+cassandra:
+	mkdir -p bin
+	cd bin && \
+	curl --silent http://ftp.wayne.edu/apache//cassandra/0.8.6/apache-cassandra-0.8.6-bin.tar.gz | tar -zvx
+	mv bin/apache-cassandra-0.8.6 bin/cassandra
+	cp etc/cassandra.yaml bin/cassandra/conf/cassandra.yaml
+
+clean-cassandra:
+	rm -rf cassandra
+
+clean:	clean-cassandra clean-env 
+
+test: 
+	$(NOSE) notifserver/tests
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MessageQueue',
+    version=0.8,
+    packages=find_packages(),
+
+    install_requires=[
+        'distribute',
+        'gunicorn',
+        'nose',
+        'pyramid',
+        'webtest',
+        'thrift',
+        'pycassa',
+    ],
+
+    entry_points="""
+        [paste.app_factory]
+        client_agent = message.clientagent:make_client_agent
+        post_office = notifserver.postoffice:make_post_office
+        post_office_router = notifserver.postoffice:make_post_office_router
+        [paste.filter_app_factory]
+        basic_auth = notifserver.auth:make_basic_auth
+    """,
+
+    test_suite = 'nose.collector',
+
+    author="Ben Bangert",
+    author_email="bbangert@mozilla.com",
+    description="Message Queue server",
+    keywords="message-queue notifications server messaging queue",
+)
