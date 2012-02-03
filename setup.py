e4626a3c0dfe70b3015560ee104d53c87585b318
Ben Bangert <ben@groovie.org>
2012-02-03 12:24:43 -0800

    Fixup for consistency

--- setup.py
+++ setup.py
@@ -1,33 +1,54 @@
+__version__ = '0.8'
+
+import os
+
 from setuptools import setup, find_packages
 
-setup(
-    name='queuey',
-    version=0.8,
-    packages=find_packages(),
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.rst')) as f:
+    CHANGES = f.read()
 
-    install_requires=[
-        'distribute',
+reqs = [
+    'distribute',
+    'nose',
+]
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    reqs.extend([
         'gunicorn',
-        'nose',
         'pyramid',
         'webtest',
         'thrift',
         'pycassa',
-    ],
+    ])
 
-    tests_requires=[
-        'mock>=0.7.2'
+setup(
+    name='queuey',
+    description="Message Queue server",
+    version=__version__,
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
     ],
-
+    keywords="message-queue notifications server messaging queue",
+    author="Ben Bangert",
+    author_email="bbangert@mozilla.com",
+    url="http://queuey.readthedocs.org/",
+    license="MPLv2.0",
+    packages=find_packages(),
+    test_suite="queuey.tests",
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=['pkginfo', 'Mock>=0.7', 'nose'],
+    install_requires=reqs,
     entry_points="""
         [paste.app_factory]
         main = queuey:main
     """,
 
-    test_suite='nose.collector',
-
-    author="Ben Bangert",
-    author_email="bbangert@mozilla.com",
-    description="Message Queue server",
-    keywords="message-queue notifications server messaging queue",
 )
