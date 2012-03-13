640ec54b60f42620217ab28662ee03a98cd830d3
Ben Bangert <ben@groovie.org>
2012-03-13 14:53:08 -0700

    robust handling of Cassandra coming online

--- runtests.py
+++ runtests.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import time
 from contextlib import contextmanager
 
 import pycassa
@@ -32,14 +33,19 @@ def main():
     host = os.environ.get('TEST_CASSANDRA_HOST', '127.0.0.1')
     hosts = parse_hosts(host)
     with supervisor():
-        setup(40)
-        try:
-            pycassa.ConnectionPool(keyspace='MessageStore', server_list=hosts)
-        except pycassa.InvalidRequestException as m:
-            if 'Keyspace MessageStore does not exist' in m.why:
-                lhost = hosts[0].split(':')[0]
-                os.system('bin/cassandra/bin/cassandra-cli -host %s --file etc/cassandra/message_schema.txt' % lhost)
-                os.system('bin/cassandra/bin/cassandra-cli -host %s --file etc/cassandra/metadata_schema.txt' % lhost)
+        while 1:
+            setup(40)
+            try:
+                pycassa.ConnectionPool(keyspace='MessageStore', server_list=hosts)
+                break
+            except pycassa.InvalidRequestException as m:
+                if 'Keyspace MessageStore does not exist' in m.why:
+                    lhost = hosts[0].split(':')[0]
+                    os.system('bin/cassandra/bin/cassandra-cli -host %s --file etc/cassandra/message_schema.txt' % lhost)
+                    os.system('bin/cassandra/bin/cassandra-cli -host %s --file etc/cassandra/metadata_schema.txt' % lhost)
+                break
+            except pycassa.AllServersUnavailable:
+                time.sleep(1)
         ret = os.system('make test-python')
     sys.exit(ret)
 
