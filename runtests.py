274ea007c28182dae61d59ea74d7faa0c4d4d9e4
Hanno Schlichting <hanno@hannosch.eu>
2012-07-18 12:31:00 +0200

    remove C* schema creation from test setup code

--- runtests.py
+++ runtests.py
@@ -5,14 +5,9 @@
 
 import os
 import sys
-import time
 from contextlib import contextmanager
 
-from thrift.transport.TTransport import TTransportException
-
 from queuey.testing import setup
-from queuey.storage.cassandra import parse_hosts
-from queuey.storage.cassandra import Schema
 
 
 @contextmanager
@@ -30,19 +25,8 @@ def supervisor():
 
 def main():
     ret = 0
-    # Check that keyspaces necessary are there, create otherwise
-    host = os.environ.get('TEST_CASSANDRA_HOST', '127.0.0.1')
-    hosts = parse_hosts(host)
     with supervisor():
         setup(40)
-        while 1:
-            try:
-                Schema(hosts[0]).install()
-                time.sleep(0.2)
-                break
-            except TTransportException:
-                print(u'Waiting on system manager for 0.1 seconds.')
-                time.sleep(0.1)
         ret = os.system('make test-python')
     sys.exit(ret)
 
